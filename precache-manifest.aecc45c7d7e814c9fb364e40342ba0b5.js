5551ac35f078d93b3c28b3b4a74a012d8fb01011
Renaud <renaud.melet@epitech.eu>
2020-07-14 05:55:55 +0200

    Updates

---
+++ precache-manifest.aecc45c7d7e814c9fb364e40342ba0b5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce6340a7af793a8c2559",
+    "url": "/Wolf3D-react-editor/4018d8a06e558ba92edb.module.wasm"
+  },
+  {
+    "revision": "3a4ba99b431cf9af4873c2807eabe040",
+    "url": "/Wolf3D-react-editor/index.html"
+  },
+  {
+    "revision": "0d64f97325ad2984eea7",
+    "url": "/Wolf3D-react-editor/static/js/2.76bc6d8e.chunk.js"
+  },
+  {
+    "revision": "7bffe171c6a70b4e7135d4da7eb821d9",
+    "url": "/Wolf3D-react-editor/static/js/2.76bc6d8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce6340a7af793a8c2559",
+    "url": "/Wolf3D-react-editor/static/js/3.ea72f01e.chunk.js"
+  },
+  {
+    "revision": "29a7a4c181d6939c9111",
+    "url": "/Wolf3D-react-editor/static/js/main.2656c05f.chunk.js"
+  },
+  {
+    "revision": "1a1e7602378a86be4f0c",
+    "url": "/Wolf3D-react-editor/static/js/runtime-main.c37d4d44.js"
+  },
+  {
+    "revision": "af50fe4d6bd30dbbaf05a77452a30c76",
+    "url": "/Wolf3D-react-editor/static/media/sprites.af50fe4d.png"
+  },
+  {
+    "revision": "a310acf401661e71ec2df9dca02bb1c3",
+    "url": "/Wolf3D-react-editor/static/media/tiles.a310acf4.png"
+  }
+]);
